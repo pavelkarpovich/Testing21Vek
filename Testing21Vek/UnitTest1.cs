836a5bb8a5506e739f7aec01dbcfdbb85cb087dc
pavelkarpovich2015 <pavelkarpovich2015@gmail.com>
2023-06-06 22:57:41 +0300

    Add project files.

---
+++ Testing21Vek/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Testing21Vek
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
